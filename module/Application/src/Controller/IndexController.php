e558064d488be81cea86b2ab2d209b57c36531b4
JamesB <jamesbotwright78@gmail.com>
2017-06-17 19:32:18 +0100

    calendar action for viewing

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -9,6 +9,7 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
 
 class IndexController extends AbstractActionController
 {
@@ -31,5 +32,13 @@ class IndexController extends AbstractActionController
     public function indexAction()
     {
         return new ViewModel();
+    }
+    
+    /**
+     * 
+     */
+    public function calendarAction()
+    {
+        return [];
     }
 }
