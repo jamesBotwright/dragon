acec7b90bf9aec9ca89e672ee3419bb2052e563f
JamesB <jamesbotwright78@gmail.com>
2017-06-18 17:09:26 +0100

    debugging

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -68,7 +68,7 @@ function postAddEventData() {
 		},
 		error: function(jXHR, textStatus, errorThrown) {
 			if (jXHR.status == 401) {
-				$("#addEventModal").html(jXHR.responseText);
+				$("#existingContactSearchForm").html(jXHR.responseText);
 			}
 		}
 	});
