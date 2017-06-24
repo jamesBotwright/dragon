559cc9c3de6b462b50131308bd0c6c0e3977cb01
JamesB <jamesbotwright78@gmail.com>
2017-06-24 15:21:03 +0100

    Swap query out

--- module/Application/src/Repository/EventsRepository.php
+++ module/Application/src/Repository/EventsRepository.php
@@ -6,21 +6,29 @@ use Doctrine\ORM\EntityRepository;
 
 class EventsRepository extends EntityRepository
 {
+    /**
+     * @author James Botwright <james.botwright@glazingvision.co.uk>
+     * @version v1.0 - 24.06.17
+     * @throws Exception
+     * @return array
+     **/
     public function getEvents()
     {
-        $qb = $this->_em->createQueryBuilder();
-        $qb->select('events')->from('Application\Entity\Events','events');
-//        ->where('events.dayOfWeekId = :dayOfWeekId')
-//        ->setParameter('dayOfWeekId', $dayOfWeekId);
-        
-        //SELECT * FROM events WHERE start >= FROM_UNIXTIME(:start) AND end < FROM_UNIXTIME(:end) ORDER BY start ASC
-        
-        try {
-            $query      = $qb->getQuery();
-            $events     = $query->getResult();
-        } catch (\Exception $e) {
-            echo 'Caught Exception: ' . $e->getMessage() . __METHOD__;
+        try{
+            $sql =
+            "SELECT *
+                FROM Events AS events
+                    
+                ORDER BY events.start ASC
+            ";
+            
+            //SELECT * FROM events WHERE start >= FROM_UNIXTIME(:start) AND end < FROM_UNIXTIME(:end) ORDER BY start ASC
+            $stmt = $this->_em->getConnection()->prepare($sql);
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+        } catch (\Exception $e){
+            echo 'Caught exception: ' . $e->getMessage() . ' - ' . __METHOD__;
         }
-        return $events;
+        return $results;
     }
 }
