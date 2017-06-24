6bf92d3a68ffc66b9af698c8884067ddada90c7d
JamesB <jamesbotwright78@gmail.com>
2017-06-24 15:28:42 +0100

    Single load effect

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,13 +1,4 @@
 /**
- * Fade spinner when loaded
- * @author James Botwright<james.botwright@glazingvision.co.uk>
- * @version v1.0 18 Jun 2017
- */
-$(window).on('load', function() {
-	$(".se-pre-con").fadeOut("slow");
-});
-
-/**
  * Bind loading GIF to AJAX events
  * @author James Botwright<james.botwright@glazingvision.co.uk>
  * @version v1.0 18 Jun 2017
@@ -44,6 +35,7 @@ function onLoadFunction() {
             };
         }
     });
+    $(".se-pre-con").fadeOut("slow");
 }
 
 /**
