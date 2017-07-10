b2306178d792e094c23fc0d3d3120bba4d40fdc6
JamesB <jamesbotwright78@gmail.com>
2017-07-10 18:46:31 +0100

    Sp.

--- module/User/src/Service/Factory/AuthAdaptorFactory.php
+++ module/User/src/Service/Factory/AuthAdaptorFactory.php
@@ -9,7 +9,7 @@ use Zend\ServiceManager\Factory\FactoryInterface;
  * This is the factory class for AuthAdapter service. The purpose of the factory
  * is to instantiate the service and pass it dependencies (inject dependencies).
  */
-class AuthAdapterFactory implements FactoryInterface
+class AuthAdaptorFactory implements FactoryInterface
 {
     /**
      * This method creates the AuthAdapter service and returns its instance. 
